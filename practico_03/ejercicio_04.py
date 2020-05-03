a3edf793e6a80697e7df3c02f012e037e8a937e8
lucas saldias <saldiaslucas@gmail.com>
2020-05-03 02:19:38 -0300

    Solucion Asserts ejercicio 4 y 5

--- practico_03/ejercicio_04.py
+++ practico_03/ejercicio_04.py
@@ -11,18 +11,20 @@ from practico_03.ejercicio_02 import agregar_persona
 def buscar_persona(id_persona):
     var_db = db.connect('practico-03.db')
     cursor = var_db.cursor()
-    query = "SELECT * FROM Persona WHERE IdPersona = %s" % id_persona
-    cursor.execute(query)
-    result = cursor.fetchall()
-    var_db.commit()
-    return False if cursor.rowcount == 0 else result
+    sql = '''SELECT * FROM persona WHERE IdPersona = ?'''
+    query_data = (id_persona,)
+    result = cursor.execute(sql, query_data).fetchone()
+    cursor.close()
+    var_db.close()
+    return result if result else False
 
 
 @reset_tabla
 def pruebas():
     juan = buscar_persona(agregar_persona('juan perez', datetime.datetime(1988, 5, 15), 32165498, 180))
-    assert juan == (1, 'juan perez', datetime.datetime(1988, 5, 15), 32165498, 180)   #No entiendo que hace
+    assert juan == (1, 'juan perez', str(datetime.datetime(1988, 5, 15)), 32165498, 180)
     assert buscar_persona(12345) is False
+
 
 if __name__ == '__main__':
     pruebas()
--- practico_03/ejercicio_05.py
+++ practico_03/ejercicio_05.py
@@ -3,6 +3,7 @@
 
 import datetime
 import sqlite3 as db
+
 from practico_03.ejercicio_01 import reset_tabla
 from practico_03.ejercicio_02 import agregar_persona
 from practico_03.ejercicio_04 import buscar_persona
@@ -11,20 +12,26 @@ from practico_03.ejercicio_04 import buscar_persona
 def actualizar_persona(id_persona, nombre, nacimiento, dni, altura):
     var_db = db.connect('practico-03.db')
     cursor = var_db.cursor()
-    naci = str(nacimiento)
-    query = "UPDATE Persona SET Nombre = \"%s\", FechaNacimiento = datetime(\"%s\") ,DNI = %i, altura = %i WHERE IdPersona = %i" %(nombre,naci,dni,altura,id_persona )
-    print(query)
-    cursor.execute(query)
-    #var_db.commit()
-    return False if cursor.rowcount == 0 else True
-
+    persona = buscar_persona(id_persona)
+    if persona:
+        sql = '''UPDATE Persona SET Nombre = ?,
+                                            FechaNacimiento = ?,
+                                            DNI = ?, 
+                                            altura = ? 
+                                            WHERE IdPersona = ?'''
+        query_data = (nombre, nacimiento, dni, altura, id_persona,)
+        cursor.execute(sql, query_data)
+        var_db.commit()
+        return cursor.lastrowid
+    return False
 
 @reset_tabla
 def pruebas():
     id_juan = agregar_persona('juan perez', datetime.datetime(1988, 5, 15), 32165498, 180)
     actualizar_persona(id_juan, 'juan carlos perez', datetime.datetime(1988, 4, 16), 32165497, 181)
-    assert buscar_persona(id_juan) == (1, 'juan carlos perez', datetime.datetime(1988, 4, 16), 32165497, 181)
+    assert buscar_persona(id_juan) == (1, 'juan carlos perez', str(datetime.datetime(1988, 4, 16)), 32165497, 181)
     assert actualizar_persona(123, 'nadie', datetime.datetime(1988, 4, 16), 12312312, 181) is False
+
 
 if __name__ == '__main__':
     pruebas()
